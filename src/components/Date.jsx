4fea722ebaaeea89238087e69e078e4e595668bc
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-06-19 22:40:54 +0530

    Project Init - Make folder structure

---
+++ src/components/Date.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Date() {
+  return (
+    <div>Date</div>
+  )
+}
---
+++ src/components/Weather.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import axios from 'axios';
+
+export default function Weather() {
+  return (
+    <div>Weather</div>
+  )
+}
