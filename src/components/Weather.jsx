272a84509fe5aa8c3b810d5ca8f31239ac05fac8
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-06-23 22:33:20 +0530

    weather api key url

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const url = `https://https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=imperial&appid=75e6f3ac0cf129d8d35cdfccdc028f7a
+`;
 
 export default function Weather() {
-  return (
-    <div>Weather</div>
-  )
+  const [data, setData] = useState(null);
+
+  return <div> </div>;
 }
