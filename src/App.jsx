2e148da63f95187d0f83fc83bed9623a1d866e7b
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-06-24 23:55:05 +0530

    Time.jsx basic done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Time from './components/Time'
 import Weather from './components/Weather'
 
 export default function App() {
   return (
     <div>
       <Weather/>
+      <Time/>
     </div>
   )
 }
--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Moment from "react-moment";
+import "moment-timezone";
 
 export default function Time() {
+  let today = new Date();
+
+  let greeting = () => {
+    if (today.getHours() >= 5 && today.getHours() < 11) {
+      return "Good Morning";
+    } else if (today.getHours() >= 11 && today.getHours() < 17) {
+      return "Good Afternoon";
+    } else if (today.getHours() >= 17 && today.getHours() < 24) {
+      return "Good Evening";
+    } else {
+      return "What are you doing up at this hour?";
+    }
+  };
+
   return (
-    <div>Time</div>
-  )
+    <div>
+      <p>
+        <Moment format="LT"></Moment>
+        <p>{greeting()}</p>
+      </p>
+    </div>
+  );
 }
