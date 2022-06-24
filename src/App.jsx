e9921b7c9e3349de25cab4d1d784b23f6bbdffa9
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-06-24 23:25:46 +0530

    Done with weather.jsx styling left

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Weather from './components/Weather'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Weather/>
+    </div>
   )
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const url = `https://https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=imperial&appid=75e6f3ac0cf129d8d35cdfccdc028f7a
-`;
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=75e6f3ac0cf129d8d35cdfccdc028f7a";
 
 export default function Weather() {
   const [data, setData] = useState(null);
 
-  return <div> </div>;
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(data);
+
+  if (!data) return null;
+
+  return <p>{data.main.temp} </p>;
 }
